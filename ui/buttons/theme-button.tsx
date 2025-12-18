be09e7ec7d6725ae7320ecd41c7b911bd6674428
bonsng <john.k7795@gmail.com>
2025-12-18 17:31:17 +0900

    first commit

---
+++ ui/buttons/theme-button.tsx
@@ -0,0 +1,25 @@
+"use client";
+
+import { Contrast } from "lucide-react";
+import { useTheme } from "next-themes";
+
+export function ThemeModeButtons() {
+  const { setTheme, theme } = useTheme();
+
+  const handleToggle = () => {
+    setTheme(theme === "dark" ? "light" : "dark");
+  };
+
+  return (
+    <button
+      onClick={handleToggle}
+      aria-label="Toggle theme"
+      className="rounded-lg p-2
+                 hover:bg-zinc-100
+                 dark:hover:bg-zinc-800
+                 transition fixed top-2 right-2"
+    >
+      <Contrast size={20} />
+    </button>
+  );
+}
